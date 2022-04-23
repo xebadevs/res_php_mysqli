9d163a8faba4140aa2dab0c212cc0246981c35f5
Sebastian Londero <xebadevs@gmail.com>
2022-04-22 23:02:49 -0300

    mysqli_1

---
+++ mysql1.php
@@ -0,0 +1,21 @@
+<?php
+
+const DB_HOSTNAME = '127.0.0.1';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
+const DB_DATABASE_NAME = 'phpoop';
+const DB_CHARSET = 'UTF8';
+
+$link = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
+mysqli_set_charset($link, DB_CHARSET);
+
+$sql = 'SELECT * FROM productos';
+$rs = mysqli_query($link, $sql);
+mysqli_close($link);
+
+$producto = mysqli_fetch_assoc($rs);
+print_r($producto);
+
+echo '<hr>';
+$producto = mysqli_fetch_assoc($rs);
+print_r($producto);
