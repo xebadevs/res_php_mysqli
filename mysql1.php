4f7e90265fd31c6065a725ba35f76ea3c39a1bd9
Sebastian Londero <xebadevs@gmail.com>
2022-04-24 16:01:48 -0300

    updates

--- mysql1.php
+++ mysql1.php
@@ -3,7 +3,7 @@
 const DB_HOSTNAME = '127.0.0.1';
 const DB_USERNAME = 'root';
 const DB_PASSWORD = '';
-const DB_DATABASE_NAME = 'phpoop';
+const DB_DATABASE_NAME = 'comercioit';
 const DB_CHARSET = 'UTF8';
 
 $link = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
